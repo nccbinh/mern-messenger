33b0050e606b986e196ac58639c03ef2c9380f87
Binh Nguyen <nccbinh@gmail.com>
2021-04-05 19:58:31 -0700

    add refs to conversation schema

--- server/models/conversation.js
+++ server/models/conversation.js
@@ -3,20 +3,20 @@
  * @author Binh Nguyen
  * @since 0.0.1
  */
- const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
- const { Schema } = mongoose;
- 
- const MessageSchema = new Schema({
-    author: { type: String, index: true },
-    content: String
- }, { timestamps: { createdAt: 'created' } });
+const { Schema } = mongoose;
 
- const ConversationSchema = new Schema({
-     // for future use if group chat is implemented
-    startedBy: { type: String, index: true },
-    participants: [String],
-    messages: [MessageSchema]
- }, { timestamps: { updatedAt: 'lastUpdated' } });
- 
- mongoose.model('Conversation', ConversationSchema);
+const MessageSchema = new Schema({
+   author: { type: Schema.Types.ObjectId, ref: 'User' },
+   content: String
+}, { timestamps: { createdAt: 'created' } });
+
+const ConversationSchema = new Schema({
+   // for future use if group chat is implemented
+   startedBy: { type: Schema.Types.ObjectId, ref: 'User' },
+   participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+   messages: [MessageSchema]
+}, { timestamps: { updatedAt: 'lastUpdated' } });
+
+mongoose.model('Conversation', ConversationSchema);
--- server/routes/api/conversation.js
+++ server/routes/api/conversation.js
@@ -17,9 +17,9 @@ const Validator = require('../../middlewares/validator');
  */
 router.get('/', Auth, Validator.searchConversation, async (req, res, next) => {
     // gets username from request
-    const username = req.user.username;
+    const userId = req.user.id;
     // gets all conversations and sorts by last updated
-    Conversation.find({ participants: username }).sort('-lastUpdated').then(
+    Conversation.find({ participants: userId }).sort('-lastUpdated').then(
         (results) => {
             // breaks if no matches
             if (!results || !results.length) {
@@ -59,7 +59,7 @@ router.get('/:id', Auth, async (req, res, next) => {
                 return res.status(422).json({ message: 'Conversation not found.' });
             }
             // checks if logged in user is one of the participants
-            if (result.participants.indexOf(req.user.username) < 0) {
+            if (result.participants.indexOf(req.user.id) < 0) {
                 return res.status(401).json({ message: 'Unauthorized' });
             }
             // sorts by last updated
@@ -80,21 +80,21 @@ router.get('/:id', Auth, async (req, res, next) => {
  * @description Start a new conversation.
  */
 router.post('/', Auth, Validator.newConversation, async (req, res, next) => {
-    const username = req.user.username;
+    const userId = req.user.id;
     const { body: { conversation } } = req;
     const recipient = await User.findOne({ username: conversation.to });
-    // checks if the user to start the conversation with exists
-    if(!recipient) {
+    // checks if all members exist
+    if(!userId || !recipient) {
         return res.status(422).json({
             errors: { to: 'Recipient not found.' }
         });
     }
     // starts a new conversation
     const conv = {
-        startedBy: username,
-        participants: [username, conversation.to],
+        startedBy: userId,
+        participants: [userId, recipient._id],
         messages: [{
-            author: username,
+            author: userId,
             content: conversation.message
         }]
     };
